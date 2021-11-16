1cb15a159f3fa393119bc1f6237e4f1b9fa5f538
Yamada Mamoru <87681995+minh189@users.noreply.github.com>
2021-11-16 08:58:39 +0700

    upload
    Myworld

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào Thưe ! 🖐🖐🖐',
+    introDesc: `Hôm nay Minh có cái này cho Thư nè, hehehe`,
+    btnIntro: 'Đâu cái gì?',
+    title: 'Bạn có thích mình không ? 😙',
+    desc: 'Thư không trả lời hoặc thoát ra, tức là m thích t đó nha :> ',
+    btnYes: 'Có chớ, thích lắm 💖',
+    btnNo: 'T đá cho một cái !',
+    question:'Thế vì sao Thư thích Minh vây? Hehehe',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Send !',
+    reply: 'Tại vì Minh đẹp trai',
+    mess: 'Minh biết mà, Minh cũng không thích người xấu. Hehe',
+    messDesc: '💬 Nhưng mà cần gì thì ib Minh đó! 💬',
+    btnAccept: 'Oke nè 💖',
+    messLink: 'https://www.facebook.com/Phvm189'
+}
